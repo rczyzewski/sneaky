fa036a2dca837208fa7e99780abaaf23bbc3e8b1
Rafal Czyzewski <rafal.czyzewski@lottoland.com>
2023-05-13 16:37:14 +0200

    releasing source and javadoc packages

--- tortilla-examples/src/test/java/io/github/rczyzewski/tortilla/TortillaExampleTest.java
+++ tortilla-examples/src/test/java/io/github/rczyzewski/tortilla/TortillaExampleTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.stream.Stream;
 
 @Slf4j
-public class TortillaExampleTest
+class TortillaExampleTest
 {
 
     private final static ObjectMapper om = new ObjectMapper();
